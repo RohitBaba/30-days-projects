c5029bfcd89edf47bd7d66ee00aef25a9fdbcccf
Rohit Pandey <rp5120523@gmail.com>
2025-04-26 19:21:46 +0530

    alert added

--- script.js
+++ script.js
@@ -8,25 +8,46 @@ const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city) {
     const responce = await fetch(apiUrl +city+ `&appid=${apiKey}`);
+if(responce.status == 404) {
+    // document.querySelector(".weather").style.display = "none";
+alert("invalid")
+
+}else {
+    document.querySelector(".weather").style.display = "block";
+    
+}
+
     var data = await responce.json();
-    console.log(data);
+
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp )+ "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+
+
+
+
 if(data.weather[0].main == "Clouds"){
-    weatherIcon.scr = "weather-images\clouds.png"
+    weatherIcon.scr = "/weather-images/cloud.png"
 }else if(data.weather[0].main == "clear"){
-    weatherIcon.src =  "weather-images\clear.png";
+    weatherIcon.src =  "/weather-images/clear.png";
 }else if(data.weather[0].main == "Rain"){
-    weatherIcon.src = "\rain.png";
+    weatherIcon.src = "/weather-images/rain.png";
 }else if(data.weather[0].main == "Drizzle"){
-    weatherIcon.src = "\drizzle.png";
+    weatherIcon.src = "/weather-images/drizzle.png";
 }else if(data.weather[0].main == "Mist"){
-    weatherIcon.src = "\mist.png";
+    weatherIcon.src = "/weather-images/mist.png";
 }
+document.querySelector(".weather").style.display = "block";
+// document.querySelector(".error").style.display = "block";
+
 }
+
+
+
+
+
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 
